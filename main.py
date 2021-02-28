213789ddfc1ffbfcb80799fc4d94dec652f0dcf8
IvanTrofimov052 <55573197+IvanTrofimov052@users.noreply.github.com>
2021-02-28 17:36:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,13 @@ import matplotlib.pyplot as plt
 plt.imshow(out)
 
 # evaluating the model 
+#from keras.models import load_model
+
+#model.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
+#del model  # deletes the existing model
+
+# returns a compiled model
+# identical to the previous one
+#model = load_model('my_model.h5')
+
 print(model.evaluate_segmentation( inp_images_dir="dataset1/images_prepped_test/"  , annotations_dir="dataset1/annotations_prepped_test/" ) )
